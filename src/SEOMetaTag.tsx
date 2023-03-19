d0d92469570534e90dd6dfd5ecc8dc1f0634775d
cobaltsquare <126875757+decablock@users.noreply.github.com>
2023-03-19 10:07:47 +0900

    update meta tags

--- src/SEOMetaTag.tsx
+++ src/SEOMetaTag.tsx
@@ -5,13 +5,12 @@ const SEOMetaTag = () => {
 		<Helmet>
 			<title>Flare</title>
 
-			<meta name="description" content="개쩌는 아티스트" />
+			<meta name="description" content="donut" />
 			<meta name="keywords" content="2D애니메이터, 아티스트" />
 
 			<meta name="theme-color" content="#7B6DFC" />
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content="Flare" />
-			<meta property="og:site_name" content="Flare" />
 			<meta property="og:description" content="donut" />
 			<meta
 				property="og:image"
@@ -28,6 +27,7 @@ const SEOMetaTag = () => {
 				name="twitter:image"
 				content={`${process.env.PUBLIC_URL}/img/thumbnail.png`}
 			/>
+			<meta name="twitter:card" content="summary_large_image" />
 
 			<link rel="apple-touch-icon" href={`%PUBLIC_URL%/logo192.png`} />
 			<link rel="manifest" href={`${process.env.PUBLIC_URL}/manifest.json`} />
